a7d09c5d71be7a38a667347cf31abdd882d5eab0
Mugundh <mugundhjb@gmail.com>
2023-10-28 22:59:41 +0530

    Created user model

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+app.listen(3000, () => {
+    console.log("Server is running!")
+})
