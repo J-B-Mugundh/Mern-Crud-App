f6f6f01c7bbe98e6f43b29b8d6735334a6517d86
Mugundh <mugundhjb@gmail.com>
2023-10-28 21:48:52 +0530

    Initial commit

---
+++ client/src/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateUser() {
+  return (
+    <div>
+      CreateUser
+    </div>
+  )
+}
+
+export default CreateUser
---
+++ client/src/UpdateUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UpdateUser() {
+  return (
+    <div>
+      Update User
+    </div>
+  )
+}
+
+export default UpdateUser
