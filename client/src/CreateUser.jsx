7f21100c60a18a3e80d758120cd12dab46668615
Mugundh <mugundhjb@gmail.com>
2023-10-29 11:36:50 +0530

    Added new user API

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  const Submit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/createUser', {name, email, age})
+    .then(res => {
+      console.log(res);
+      navigate('/');
+    })
+    .catch(err => console.log(err))
+  }
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
     <div className='w-50 bg-white rounded p-3'>
-    <form>
-  <h2>Add User</h2>
-  <div className="mb-2">
-    <label htmlFor="">Name</label>
-    <input type="text" placeholder='Enter Name' className='form-control'/>
-  </div>
-  <div className="mb-2">
-    <label htmlFor="">Email</label>
-    <input type="email" placeholder='Enter Email' className='form-control'/>
-  </div>
-  <div className="mb-2">
-    <label htmlFor="">Age</label>
-    <input type="text" placeholder='Enter Age' className='form-control'/>
-  </div>
-  <button className='btn btn-success'>Submit</button>
-</form>
+    <form onSubmit={Submit}>
+      <h2>Add User</h2>
+      <div className="mb-2">
+        <label htmlFor="">Name</label>
+        <input type="text" placeholder='Enter Name' className='form-control'
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Email</label>
+        <input type="email" placeholder='Enter Email' className='form-control'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="">Age</label>
+        <input type="text" placeholder='Enter Age' className='form-control'
+          onChange={(e) => setAge(e.target.value)}
+        />
+      </div>
+      <button className='btn btn-success'>Submit</button>
+    </form>
     </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
 app.listen(3000, () => {
     console.log("Server is running!")
 })
